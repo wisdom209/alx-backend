1b419129693ba8b708671931c3aac96f1e0bdbd0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-26 13:52:45 +0100

    Added 1-fifo_cache.py 1-main.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Caching module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO caching class"""
+
+    key_queue = []
+
+    def __init__(self):
+        """initialize """
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item to cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                    and key not in self.cache_data:
+                if (len(self.key_queue) > 0):
+                    del self.cache_data[self.key_queue[0]]
+                    print(f"DISCARD {self.key_queue[0]}")
+                    self.key_queue.pop(0)
+            self.cache_data[key] = item
+            self.key_queue.append(key)
+
+    def get(self, key):
+        """get from cache"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
