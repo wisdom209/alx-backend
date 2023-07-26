463279617596c3fb5df815b0992acb7be3c86d74
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-26 15:43:17 +0100

    Added 3-lru_cache.py 4-mru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Caching module"""
+from collections import deque
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU caching class"""
+
+    def __init__(self):
+        """initialize """
+        super().__init__()
+        self.key_queue = []
+        self.lru_index = 0
+
+    def put(self, key, item):
+        """Add an item to cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                    and key not in self.cache_data:
+                lr_key = self.key_queue.pop(0)
+                del self.cache_data[lr_key]
+                print(f"DISCARD {lr_key}")
+        if key in self.cache_data:
+            self.key_queue.remove(key)
+        self.cache_data[key] = item
+        self.key_queue.append(key)
+
+    def get(self, key):
+        """get from cache"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Caching module"""
+from collections import deque
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """LRU caching class"""
+
+    def __init__(self):
+        """initialize """
+        super().__init__()
+        self.key_queue = []
+
+    def put(self, key, item):
+        """Add an item to cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                    and key not in self.cache_data:
+                mr_key = self.key_queue.pop()
+                del self.cache_data[mr_key]
+                print(f"DISCARD {mr_key}")
+        if key in self.cache_data:
+            self.key_queue.remove(key)
+        self.cache_data[key] = item
+        self.key_queue.append(key)
+
+    def get(self, key):
+        """get from cache"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
