4b40fad3635e96c792a20e2067a66064538d8cfe
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-26 14:22:31 +0100

    Added 1-fifo_cache.py 1-main.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,26 +5,25 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFO caching class"""
+    """FIFO Cache implementation"""
 
     def __init__(self):
-        """initialize """
+        """initialise the init class"""
         super().__init__()
         self.queue = deque()
 
     def put(self, key, item):
-        """Add an item to cache"""
-        if key is not None and item is not None\
-                and key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                first = self.queue.popleft()
-                self.cache_data.pop(first)
-                print(f"DISCARD {first}")
-            self.cache_data[key] = item
-            self.queue.append(key)
+        """method that puts the key-value pairs in the FIFO algo"""
+        if key is not None and item is not None and key not in self.cache_data:
+            if len(self.cache_data) >= Base.MAX_ITEMS:
+                oldest_item = self.queue.popleft()
+                self.cache_data.pop(oldest_item)
+                print("DISCARD: {}".format(oldest_item))
+        self.cache_data[key] = item
+        self.queue.append(key)
 
     def get(self, key):
-        """get from cache"""
+        """get the values in the FIFO algorithm"""
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
