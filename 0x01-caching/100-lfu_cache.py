43da1732ba28584cddfa382fc467a12dee89f011
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-27 06:08:38 +0100

    Added 3-lru_cache.py 4-mru_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,48 +1,55 @@
 #!/usr/bin/env python3
 """LFU caching"""
 from collections import defaultdict, deque
-Base = __import__('base_caching').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LFUCache(Base):
-    """LFU Cache implementation"""
+class LFUCache(BaseCaching):
+    """LFU Caching"""
 
     def __init__(self):
-        """initialise the init class"""
+        """initialise class"""
         super().__init__()
         self.freq = defaultdict(int)
-        self.freq_items = defaultdict(deque)
+        self.freq_deque_dict = defaultdict(deque)
 
     def put(self, key, item):
-        """method that puts the key-value pairs in the LFU algo"""
+        """add item"""
         if key is not None and item is not None:
             if key in self.cache_data:
                 curr_freq = self.freq[key]
-                self.freq_items[curr_freq].remove(key)
+                self.freq_deque_dict[curr_freq].remove(key)
                 self.freq[key] += 1
             else:
-                if len(self.cache_data) >= Base.MAX_ITEMS and key not in self.cache_data:  # nopep8
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                        and key not in self.cache_data:
                     min_freq = min(self.freq.values())
 
-                    while self.freq_items[min_freq] and len(self.cache_data) >= Base.MAX_ITEMS:  # nopep8
-                        key_to_pop = self.freq_items[min_freq].popleft()
-                        print("DISCARD: {}".format(key_to_pop))
-                        del self.cache_data[key_to_pop]
-                        del self.freq[key_to_pop]
+                    while self.freq_deque_dict[min_freq] and\
+                            len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                        for_removal_key = self.freq_deque_dict[min_freq].popleft()  # nopep8
+                        print(f"DISCARD: {for_removal_key}")
+                        self.cache_data.pop(for_removal_key)
+                        self.freq.pop(for_removal_key)
 
                 self.cache_data[key] = item
                 self.freq[key] += 1
-            self.freq_items[self.freq[key]].append(key)
+            self.freq_deque_dict[self.freq[key]].append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """get the values in the LFU algorithm"""
+        """get item
+        """
         if key is not None and key in self.cache_data:
-            curr_freq = self.freq[key]
-            next_freq = curr_freq + 1
-            self.freq[key] = next_freq
+            curr = self.freq[key]
+            next = curr + 1
+            self.freq[key] = next
 
-            self.freq_items[curr_freq].remove(key)
-            self.freq_items[next_freq].append(key)
+            self.freq_deque_dict[curr].remove(key)
+            self.freq_deque_dict[next].append(key)
+
+            # Return the value associated with the key
             return self.cache_data[key]
+
+        # Key is not in the cache, return None
         return None
