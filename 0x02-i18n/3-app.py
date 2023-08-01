2acb118264fe5c805369c006e44b42726a59ed17
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-01 22:28:49 +0100

    add 3-add.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,10 +25,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """Route function"""
-    home_title = _("home_title")
-    home_header = _("home_header")
-    return render_template("3-index.html", home_title=home_title,
-                           home_header=home_header)
+    return render_template("3-index.html")
 
 
 if __name__ == '__main__':
