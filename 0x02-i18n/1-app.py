51288073b58ec7fc61d2ed41b40a4ac20f68db5c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-01 20:40:27 +0100

    added 1-app.py, templates/1-index.html

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class config:
+    """config classs"""
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config["BABEL_DEFAULT_LOCALE"] = "en"
+app.config["BABEL_DEFAULT_TIMEZONE"] = "UTC"
+babel = Babel(app)
+
+
+def index():
+    """Route function"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
