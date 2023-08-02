dce970301851a173dce97be738455c7af5ed7114
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-02 13:13:23 +0100

    update

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-"""Flask app"""
+#!/usr/bin/env python3
+"""Basic Babel setup"""
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext as _
+from flask_babel import Babel
 
 
-class Config:
-    """config classs"""
+class Config(object):
+    """config class to configure available languages"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -15,6 +16,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -23,37 +25,38 @@ users = {
 }
 
 
+@babel.localeselector
+def get_locale():
+    """get the locale lang"""
+    lang = request.args.get("locale")
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 def get_user(login_as):
-    """get a user"""
-    if login_as is None:
+    """return user dict"""
+    if not login_as:
         return None
     return users.get(int(login_as))
 
 
 @app.before_request
 def before_request():
-    """Before a request"""
-    login_as = request.args.get('login_as')
+    """pre hook before request"""
+    login_as = request.args.get("login_as")
     g.user = get_user(login_as)
-
-
-@babel.localeselector
-def get_locale():
-    """get locale"""
-    lang = request.args.get('locale')
-    if lang in app.config['LANGUAGES']:
-        return lang
-    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Route function"""
+    """route index"""
     username = None
+    print(f"user {g.user}")
     if g.user:
         username = g.user.get('name')
-    return render_template("5-index.html", username=username)
+    return render_template('5-index.html', username=username)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
