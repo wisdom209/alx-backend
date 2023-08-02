c26621b4926a7dca976c1557695e9d78258e0112
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-02 13:16:28 +0100

    update

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Babel setup"""
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -17,14 +17,6 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
 @babel.localeselector
 def get_locale():
     """select the best match for supported languages"""
@@ -34,29 +26,10 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-def get_user(login_as):
-    """function that returns a user dictionary or None if th ID is not found"""
-    user_id = int(login_as) if login_as else None
-    if user_id in users:
-        return users[user_id]
-    return None
-
-
-@app.before_request
-def before_request():
-    """use get_user to find a user if any"""
-    login_as = request.args.get("login_as")
-    g.user = get_user(login_as)
-
-
 @app.route('/', strict_slashes=False)
-def index():
+def index(locale="fr"):
     """index function"""
-    username = None
-    print(f"user {g.user}")
-    if g.user:
-        username = g.user.get('name')
-    return render_template('5-index.html', username=username)
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
